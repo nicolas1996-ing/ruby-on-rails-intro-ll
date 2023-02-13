13c1cd7af679ff74d43be951cb9d68424bb8cddb
jose_nicolas_aristizabal_ramirez <josenicolasaristizabalramirez@gmail.com>
2023-02-13 15:25:52 -0500

    feat: config-routing and tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+
+  #  http://127.0.0.1:3000/static_pages/home
+  #  http://127.0.0.1:3000/static_pages/help
+
+  # primera pag que ve el usuario
+  root "static_pages#home"
+
+  # rutas relativas a root ... 
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "application#hello"
+  # root "application#hello"
   # Defines the root path route ("/")
   # root "articles#index"
 end
