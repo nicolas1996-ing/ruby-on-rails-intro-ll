1eb579ced27e56784967d1ade5294c28a97ff7e5
jose_nicolas_aristizabal_ramirez <josenicolasaristizabalramirez@gmail.com>
2023-02-27 17:53:18 -0500

    feat: update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # Create a main sample user.
+
+# Create a main sample user.
 User.create!(name:  "Example User",
     email: "example@railstutorial.org",
     password:              "foobar",
-    password_confirmation: "foobar"),
-    admin: true
+    password_confirmation: "foobar",
+    admin: true)
 
 # Generate a bunch of additional users.
 99.times do |n|
